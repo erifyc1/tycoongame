7ea57e8104951a315302e694bba4c93e13dd738f
Jason Xu <Xinwen31415@gmail.com>
2021-02-16 23:31:39 -0600

    Started camera movement

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+		transform.position = new Vector3(0, 1, -10);
+		transform.rotation = Vector3.Down;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.W) == true)
+		{
+			transform.position += new Vector3(1, 0, 0);
+		}
+		if (Input.GetKeyDown(KeyCode.S) == true)
+		{
+			transform.position += new Vector3(-1, 0, 0);
+		}
+	}
+}
