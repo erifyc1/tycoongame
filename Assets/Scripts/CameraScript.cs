f1ce2d0e50cd0689595e0fda5d7dad049aac8267
Jason Xu <Xinwen31415@gmail.com>
2021-02-17 01:14:11 -0600

    added max zoom

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+	[SerializeField]
+	private float zoom = 10f;
+
 	// Start is called before the first frame update
 	void Start()
 	{
@@ -13,13 +16,15 @@ public class CameraScript : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		float vDir = Input.GetAxis("Vertical") * Time.deltaTime * 50;
-		float hDir = Input.GetAxis("Horizontal") * Time.deltaTime * 50;
+		float vDir = Input.GetAxis("Vertical") * Time.deltaTime * 50 * Mathf.Sqrt(zoom / 10);
+		float hDir = Input.GetAxis("Horizontal") * Time.deltaTime * 50 * Mathf.Sqrt(zoom / 10);
 		transform.position += new Vector3(hDir, 0, vDir);
 	}
 
 	void OnGUI()
 	{
-		transform.position += new Vector3(0, Input.mouseScrollDelta.y * -5, 0);
+		float zoomChange = Input.mouseScrollDelta.y * -5;
+		zoom = (zoom + zoomChange < 0) ? 0 : zoom + zoomChange;
+		transform.position = new Vector3(transform.position.x, zoom, transform.position.z);
 	}
 }
