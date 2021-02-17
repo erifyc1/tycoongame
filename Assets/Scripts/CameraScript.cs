81bb7c8ecd61598b4a1833a80d13b7343596d8b7
Jason Xu <Xinwen31415@gmail.com>
2021-02-17 00:28:52 -0600

    added camera movement/zoom

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        transform.position = new Vector3(0, 10, 0);
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		transform.position = new Vector3(0, 10, 0);
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        float vDir = Input.GetAxis("Vertical") * Time.deltaTime * 10;
-        float hDir = Input.GetAxis("Horizontal") * Time.deltaTime * 10;
-        transform.position += new Vector3(hDir, 0, vDir);
-    }
+	// Update is called once per frame
+	void Update()
+	{
+		float vDir = Input.GetAxis("Vertical") * Time.deltaTime * 50;
+		float hDir = Input.GetAxis("Horizontal") * Time.deltaTime * 50;
+		transform.position += new Vector3(hDir, 0, vDir);
+	}
+
+	void OnGUI()
+	{
+		transform.position += new Vector3(0, Input.mouseScrollDelta.y * -5, 0);
+	}
 }
