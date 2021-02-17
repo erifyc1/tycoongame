c0989547296ace5acfdad7bfcf66f18056318116
Jason Xu <Xinwen31415@gmail.com>
2021-02-17 02:56:24 -0600

    fixed camera error thingy

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -5,31 +5,30 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
 
-    private bool placed = false;
-    [SerializeField]
-    Vector3 mousePos;
-    private float closestX;
-    private float closestZ;
-    void Update()
-    {
-        if (!placed)
-        {
+	private bool placed = false;
+	[SerializeField]
+	Vector3 mousePos;
+	private float closestX;
+	private float closestZ;
 
-            mousePos = new Vector3(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, 0, Camera.main.ScreenToWorldPoint(Input.mousePosition).z);
-            Debug.Log(mousePos);
+	void OnGUI()
+	{
+		if (!placed)
+		{
+			Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Event.current.mousePosition.x, Camera.main.pixelHeight - Event.current.mousePosition.y, Camera.main.transform.position.y));
+			Debug.Log(point);
 
+			if ((point.x % 10 < 2 || point.x % 10 > 8) && (point.y % 10 < 2 || point.y % 10 > 8))
+			{
+				closestX = point.x % 10 < 2 ? Mathf.FloorToInt(point.x / 10) * 10 : point.x % 10 > 8 ? Mathf.CeilToInt(point.x / 10) * 10 : point.x;
+				closestZ = point.z % 10 < 2 ? Mathf.FloorToInt(point.z / 10) * 10 : point.z % 10 > 8 ? Mathf.CeilToInt(point.z / 10) * 10 : point.z;
+				transform.position = new Vector3(closestX, 0, closestZ);
+			}
+			else
+			{
+				transform.position = point;
+			}
 
-            if ((mousePos.x % 10 < 2 || mousePos.x % 10 > 8) && (mousePos.y % 10 < 2 || mousePos.y % 10 > 8))
-            {
-                closestX = mousePos.x % 10 < 2 ? Mathf.FloorToInt(mousePos.x / 10) * 10 : mousePos.x % 10 > 8 ? Mathf.CeilToInt(mousePos.x / 10) * 10 : mousePos.x;
-                closestZ = mousePos.z % 10 < 2 ? Mathf.FloorToInt(mousePos.z / 10) * 10 : mousePos.z % 10 > 8 ? Mathf.CeilToInt(mousePos.z / 10) * 10 : mousePos.z;
-                transform.position = new Vector3(closestX, 0, closestZ);
-            }
-            else
-            {
-                transform.position = mousePos;
-            }
-
-        }
-    }
+		}
+	}
 }
