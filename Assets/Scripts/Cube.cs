6aaaead362ccfc3085eeed3d917eab6fb7bf3bc9
Jason Xu <Xinwen31415@gmail.com>
2021-02-18 18:58:44 -0600

    fixed broken stuff

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -16,30 +16,4 @@ public class Cube : MonoBehaviour
     {
         buildUI = GameObject.FindGameObjectWithTag("buildmanager").GetComponent<BuildUI>();
     }
-
-    void OnGUI()
-    {
-        if (!placed)
-        {
-            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Event.current.mousePosition.x, Camera.main.pixelHeight - Event.current.mousePosition.y, Camera.main.transform.position.y));
-            Debug.Log(point);
-
-            closestX = point.x % 10 < 5 ? Mathf.FloorToInt(point.x / 10) * 10 : Mathf.CeilToInt(point.x / 10) * 10;
-            closestZ = point.z % 10 < 5 ? Mathf.FloorToInt(point.z / 10) * 10 : Mathf.CeilToInt(point.z / 10) * 10;
-            transform.position = new Vector3(closestX, 0, closestZ);
-            Cursor.visible = false;
-
-
-        }
-    }
-
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
-        {
-            placed = true;
-            Cursor.visible = true;
-            buildUI.placingObject = false;
-        }
-    }
 }
