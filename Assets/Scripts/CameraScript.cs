a1b6e3d6faae2659eb0fb449f4f5e1fb078cbdfc
Jacob Stolker <stolkerj@yahoo.com>
2021-02-25 14:38:43 -0600

    new camera movement part 1

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+
 	[SerializeField]
 	private float zoom = 10f;
 	private float vert;
 	private float horiz;
+
+	private float speed = 20f;
+
+	Vector2 deltaPos;
+
+	private Vector3 CameraRotation;
+
 	private Vector2 scroll;
 
     public void OnVertical(InputAction.CallbackContext context)
@@ -25,7 +33,24 @@ public class CameraScript : MonoBehaviour
         
     }
 
-		public void OnScroll(InputAction.CallbackContext context)
+	public void OnPanVert(InputAction.CallbackContext context)
+	{
+
+	}
+
+	public void OnPanHoriz(InputAction.CallbackContext context)
+	{
+
+	}
+
+	public void OnDeltaMouse(InputAction.CallbackContext context)
+    {
+
+        deltaPos = context.ReadValue<Vector2>();
+    }
+	
+
+	public void OnScroll(InputAction.CallbackContext context)
     {
         
         scroll = context.ReadValue<Vector2>();
@@ -34,21 +59,35 @@ public class CameraScript : MonoBehaviour
 	// Start is called before the first frame update
 	void Start()
 	{
-		transform.position = new Vector3(0, 10, 0);
+		Cursor.lockState = CursorLockMode.Confined;
+		transform.position = new Vector3(0, 50, 0);
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		float vDir = vert * Time.deltaTime * 50 * Mathf.Sqrt(zoom / 10);
-		float hDir = horiz * Time.deltaTime * 50 * Mathf.Sqrt(zoom / 10);
-		transform.position += new Vector3(hDir, 0, vDir);
+		
+		float RotationX = speed * deltaPos.x * Time.deltaTime;
+		float RotationY = speed * deltaPos.y * Time.deltaTime;
+		
+		CameraRotation = Camera.main.transform.rotation.eulerAngles;
+		
+		CameraRotation.x -= RotationY;
+		CameraRotation.y += RotationX;
+		
+		Camera.main.transform.rotation = Quaternion.Euler(CameraRotation);
+
+
+	}
+	private void FixedUpdate()
+	{
+		transform.position += transform.forward*vert + transform.right*horiz;
 	}
 
 	void OnGUI()
 	{
-		float zoomChange = scroll.y / (-100);
-		zoom = (zoom + zoomChange < 10f) ? 10f : zoom + zoomChange;
-		transform.position = new Vector3(transform.position.x, zoom, transform.position.z);
+		// float zoomChange = scroll.y / (-100);
+		// zoom = (zoom + zoomChange < 10f) ? 10f : zoom + zoomChange;
+		// transform.position = new Vector3(transform.position.x, zoom, transform.position.z);
 	}
 }
